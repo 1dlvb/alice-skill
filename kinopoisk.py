24fd2e9c932cbafb034c75db1bcf4951799bc112
1dlvb <matushkin-2017@mail.ru>
2022-04-21 13:42:11 +0400

    Added new functions get_genre_id and get_list_of_movies.

--- kinopoisk.py
+++ kinopoisk.py
@@ -9,7 +9,7 @@ import codecs
 
 api_client = KinopoiskApiClient(cfg("API_TOKEN"))
 path_for_genres_file = 'movie_genres.txt'
-#
+
 # request = FilmSearchByFiltersRequest()
 # request.genres = ['боевик', ]
 # request.order = FilterOrder.RATING
@@ -19,34 +19,44 @@ path_for_genres_file = 'movie_genres.txt'
 # print(response)
 
 
-def answers_and_requests(path):
-    """НЕ ЗАБЫТЬ ДОБАВИТЬ ВЫВОД ЖАНРА ФИЛЬМА/АКТИВАЦИОННОГО СЛОВА"""
-    hello_words, goodbye_words, act_movies_words, movie_genres = [], [], [], []
-
-    with codecs.open(path, 'r', encoding='utf-8') as f:
-        for i in f:
-            if i.split('1')[0] == '':
-                movie_genres.append(i.split('1')[1])
-
-        # deleting useless characters
-        for i in range(len(movie_genres)):
-            movie_genres[i] = movie_genres[i][:-2]
-
-    return movie_genres
-
-
 def get_genres(path):
     with codecs.open(path, 'r', encoding='utf-8') as f:
         return [i[:-2] for i in f]
 
 
 # print(get_genres(path='movie_genres.txt'))
-# request = FiltersRequest()
-# response = api_client.films.send_filters_request(request)
 
 
+# returns the id of a chosen genre from a genre name
 def get_genre_id(name):
+    try:
+        request = FiltersRequest()
+        response = api_client.films.send_filters_request(request)
+    except:
+        return None
     if name in get_genres(path_for_genres_file):
+        for item in response.genres:
+            if name == item.genre:
+                return item
+
+        # print(response.genres.FilterGenre.(name))
         print(f'you chose {name} genre!')
 
-get_genre_id('драму')
+
+def get_list_of_movies(genre_name, rating_from=7, year_from=1900):
+    request = FilmSearchByFiltersRequest()
+    request.year_from = year_from
+    request.rating_from = rating_from
+    request.add_genre(get_genre_id(genre_name))
+    request.order = FilterOrder.NUM_VOTE
+
+    response = api_client.films.send_film_search_by_filters_request(request)
+
+    for i in response.items:
+        if i.name_ru is not None:
+            print(i.name_ru)
+        else:
+            print(i.name_original)
+
+
+get_list_of_movies('драма', year_from=2022)
