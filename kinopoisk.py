a6a0f3cf2d821079ddc0f1c2a11bbede22adb585
1dlvb <matushkin-2017@mail.ru>
2022-04-27 19:42:26 +0400

    Bugs fixed. Speed was optimized.

--- kinopoisk.py
+++ kinopoisk.py
@@ -7,16 +7,13 @@ from kinopoisk_unofficial.request.films.filters_request import FiltersRequest
 from decouple import config as cfg
 import codecs
 from random import randint
+from act_dict import base_dict
 
 api_client = KinopoiskApiClient(cfg("API_TOKEN"))
-path_for_genres_file = 'movie_genres.txt'
+path_for_genres_file = base_dict()['movie_genres']
 
 
 class ShowMovies:
-    # returns name of genres from txt file
-    def get_genres(self, path):
-        with codecs.open(path, 'r', encoding='utf-8') as f:
-            return [i[:-2] for i in f]
 
     def get_genre_id(self, name):
         request = FiltersRequest()
@@ -30,15 +27,12 @@ class ShowMovies:
     def get_list_of_movies(self, genre_name, rating_from=7, year_from=1900):
         request = FilmSearchByFiltersRequest()
         request.year_from = year_from
-        request.rating_from = rating_from
         request.order = FilterOrder.NUM_VOTE
 
-        request.page = randint(1, 15)
+        request.page = randint(1, 10)
         response = api_client.films.send_film_search_by_filters_request(request)
-        genre = response.items
-        for i in range(len(genre)):
-            genre = response.items[i].genres
-            for k in range(len(genre)):
+        for i in range(len(response.items)):
+            for k in range(len(response.items[i].genres)):
                 genre = response.items[i].genres[k].__class__(genre=genre_name)
 
         for film in response.items:
