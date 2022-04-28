8e64f56c8347735180ea1ec71d31575f1a54098e
1dlvb <matushkin-2017@mail.ru>
2022-04-28 16:33:50 +0400

    Added new functionality to program. Added screen checker.

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ bye_answer = word_list['bye_words']
 act_movies_words = word_list['movies_activation_words']  # слова для вызова показа фильма
 genres = word_list['movie_short_genres']  # корни жанров фильмов
 genre_list = word_list['movie_genres']  # жанры
-
+hello_word_req = word_list['hello_words_from_user']  # возможные слова приветствия от юзера
+goodbye_word_req = word_list['goodbye_words_from_user']  # возможные слова прощания от юзера
+misunderstands = word_list['misunderstands']
 
 def act_movie_checker(text, act_movies, movie_genres):
     movie_genre_in_req = False
@@ -34,25 +36,33 @@ def act_movie_checker(text, act_movies, movie_genres):
 @app.route('/alice', methods=['POST'])
 def resp():
     text = request.json.get('request', {}).get('command')
+
+    try:
+        user_screen = request.json['meta']['interfaces']['screen']
+        is_user_have_screen = True
+    except KeyError:
+        is_user_have_screen = False
+
     end = False
     movie_img = None
     genre_name = None
 
-    # possible user requests and alice answers
-    bye_word_req = ['пока', 'пока-пока', 'покеда', 'до встречи', 'до скорых встреч', 'ну все, пока', 'выход',
-                    'выйти', 'все пока']
-    hello_word_req = ['привет', 'Здравсвуйте', 'здравствуй', 'привет-привет', 'приветик', 'здорова',
-                      'добрый вечер', 'добрый день', 'доброе утро']
-
     specified_genre = act_movie_checker(text=text, act_movies=act_movies_words, movie_genres=genres)[-1]
 
     # checking for a goodbye request
-    if text in bye_word_req:
+    if text in goodbye_word_req:
         response_text = f'{random.choice(bye_answer)}'
         end = True
 
     # if only the genre value is set in the request -> elif will return func only for a genres
     elif act_movie_checker(text=text, act_movies=act_movies_words, movie_genres=genres)[0] is True:
+        if is_user_have_screen:
+            tts_response = base_dict()['user_have_screen_resp']
+            response_text = 'Кажется нашла!'
+        else:
+            tts_response = base_dict()['user_havent_screen_resp']
+            response_text = 'Хм...'
+
         for genre in genre_list:
             if specified_genre in genre:
                 genre_name = genre
@@ -60,51 +70,49 @@ def resp():
         if genre_name is None:
             response_text = 'Похоже, что такого жанра нет в моём списке!'
 
-        movies = sm.get_list_of_movies(genre_name)
-        for item in movies:
-            movie_img_url = item.poster_url_preview
+        movie = sm.get_list_of_movies(genre_name)
+        try:
             movie_img = upload_image(
                 skill_id=cfg('SKILL_ID'),
                 oauth_token=cfg("YANDEX_AUTH_TOKEN"),
-                image_path_or_url=movie_img_url,
+                image_path_or_url=movie.poster_url_preview,
             )
-            m_title = item.name_ru
-            m_rating = item.rating_kinopoisk
-            if item.type.name == 'FILM':
+
+            if movie.type.name == 'FILM':
                 m_type = 'фильм'
-            elif item.type.name == 'TV_SERIES':
+            elif movie.type.name == 'TV_SERIES':
                 m_type = 'сериал'
 
-                response_text = 'Кажется я нашла, что же вам посмотреть!'
             else:
                 response_text = 'Извините, произошла какая-то ошибка.'
-            break
-        response_text = 'Похоже, что возникла какая-то ошибка! Попробуйте какой-нибудь другой жанр.'
+        except AttributeError:
+            response_text = 'Похоже, что возникла какая-то ошибка! Попробуйте какой-нибудь другой жанр.'
 
     # checking for a hello request
     elif text in hello_word_req:
         response_text = f'{random.choice(hello_answer)}'
 
-    else:
-        response_text = 'Скажите что-нибудь интересное, и я вам как-нибудь интересно отвечу!'
+    elif text:
+        response_text = misunderstands[random.randint(0, len(misunderstands)-1)]
 
-    if movie_img:
+    else:
+        response_text = 'Выберите какой-нибудь жанр фильма!'
+
+    if movie_img and tts_response and response_text:
         response = {
             'response': {
                 'text': response_text,
-                "tts": f"Ищу, что же вам посмотреть... sil <[1700]>  Уже почти нашла... sil <[1700]>"
-                       f" Готово! sil <[950]> Попробуйте глянуть {m_title}. По данным Кинопоика этот {m_type}"
-                       f" набрал {m_rating} баллов!",
+                "tts": f"{tts_response[random.randint(0, len(tts_response) - 1)]} Попробуйте глянуть {movie.name_ru}. По данным Ай Эм Ди Би этот {m_type}"
+                       f" набрал {movie.rating_imdb} баллов!",
                 'buttons': [
                     {'title': 'Привет!', 'hide': True},
-                    {'title': 'Я хочу посмотреть фильм!', 'hide': True},
                     {'title': 'Пока!', 'hide': True},
                 ],
                 'card': {
                     'type': "BigImage",
                     'image_id': movie_img['image']['id'],
-                    'title': m_title,
-                    'description': f'Рейтинг на IMDB: {m_rating}',
+                    'title': movie.name_ru,
+                    'description': f'Рейтинг на IMDB: {movie.rating_imdb}',
 
                 },
                 'end_session': end,
@@ -118,7 +126,6 @@ def resp():
                 'text': response_text,
                 'buttons': [
                     {'title': 'Привет!', 'hide': True},
-                    {'title': 'Я хочу посмотреть фильм!', 'hide': True},
                     {'title': 'Пока!', 'hide': True},
                 ],
 
